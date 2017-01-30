2a01231cf5f2ed174dcf0de5e8f563d3100e1fce
Dan Pelensky <dan@pelensky.com>
2017-01-30 14:31:46 +0000

    changed Brain to be a class constant

--- FizzBuzz/FizzBuzz/Brain.swift
+++ FizzBuzz/FizzBuzz/Brain.swift
@@ -11,7 +11,20 @@ import Foundation
 class Brain: NSObject{
 
     func isDivisibleByThree(number: Int) -> Bool {
-        return true
+        if number % 3 == 0 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func isDivisibleByFive(number: Int) -> Bool {
+        if number % 5 == 0 {
+            return true
+        } else {
+            return false
+        }
+        
     }
 
 }
--- FizzBuzz/FizzBuzzTests/BrainTests.swift
+++ FizzBuzz/FizzBuzzTests/BrainTests.swift
@@ -10,11 +10,27 @@ import XCTest
 @testable import FizzBuzz
 
 class BrainTests: XCTestCase {
+     let brain = Brain()
     
     func testIsDivisibleByThree() {
-    let brain = Brain()
         let result = brain.isDivisibleByThree(number: 3)
         XCTAssertEqual(result, true)
+    }
+    
+    func testIsNotDivisibleByThree() {
+        let result = brain.isDivisibleByThree(number: 4)
+        XCTAssertEqual(result, false)
+    }
+    
+    func testIsDivisibleByFive() {
+        let result = brain.isDivisibleByFive(number: 5)
+        XCTAssertEqual(result, true)
+    }
+    
+
+    func testIsNotDivisibleByFive() {
+        let result = brain.isDivisibleByFive(number: 4)
+        XCTAssertEqual(result, false)
     }
 }
 
